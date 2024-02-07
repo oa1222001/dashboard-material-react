63b73835f1d52955cfbc0ed5499c062cd31b3bf2
Omar Allam <41598886+oa1222001@users.noreply.github.com>
2024-02-07 13:45:40 +0200

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import { createContext } from "react"
 
-export const BACKEND_URL = "https://amad-ecommerce.onrender.com"
+export const BACKEND_URL = "http://54.162.40.240:8000"
 
 export const accountContext = createContext(null)
